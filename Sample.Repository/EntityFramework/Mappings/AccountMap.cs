e1a8647eb9178356b879eb74451dd0655e9f1f71
杜江龙 <djlnet@outlook.com>
2016-12-11 15:17:39 +0800

    .

---
+++ Sample.Repository/EntityFramework/Mappings/AccountMap.cs
@@ -0,0 +1,20 @@
+﻿using Sample.Repository.EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Repository.EntityFramework.Mappings
+{
+    public class AccountMap : EntityTypeConfiguration<Account>
+    {
+        public AccountMap()
+        {
+            this.HasKey(x => x.ID);
+            this.Property(x => x.Name).IsRequired().HasMaxLength(32);
+            this.Property(x => x.Password).IsRequired().HasMaxLength(32);
+        }
+    }
+}
---
+++ Sample.Repository/EntityFramework/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using Sample.Core.DataBaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Repository.EntityFramework.Models
+{
+    public class Account : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
